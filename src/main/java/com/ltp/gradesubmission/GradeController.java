14222d61d8d27aa5861c64dc0ec57117660d1779
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-08-03 15:11:15 +0900

    updated main

--- src/main/java/com/ltp/gradesubmission/GradeController.java
+++ src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -1,17 +1,24 @@
 package com.ltp.gradesubmission;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GradeController {
-  
+
+  List<Grade> studentGrades = Arrays.asList(
+    new Grade("Harry", "Potions", "C-"),
+    new Grade("Hermione", "Arithmency", "A+"),
+    new Grade("Neville", "Charms", "A-")
+  );
 
   @GetMapping("/grades")
   public String getGrades(Model model) {
-    Grade grade = new Grade("Harry", "Potions", "C-");
-    model.addAttribute("grade", grade);
+    model.addAttribute("grades", studentGrades);
     return "grades";
   }
 }
